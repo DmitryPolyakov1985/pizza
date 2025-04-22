b051c7463f332c6f816091815e4c7bc04f4a0579
Dmitry <dmitry@smilecdr.com>
2025-04-21 23:42:08 -0600

    flex and grid layout setup

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="hidden text-sm font-semibold">Dmitry</div>;
+  return <div className="hidden text-sm font-semibold md:block">Dmitry</div>;
 }
 
 export default Username;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase shadow-md">
+    <header className="flex items-center justify-between border-b border-stone-300 bg-yellow-500 px-4 py-3 uppercase shadow-md sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
