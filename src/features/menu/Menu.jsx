2f1bad3d374d16c9e8534801028662a1112881e0
Dmitry <dmitry@smilecdr.com>
2025-04-27 23:32:45 -0600

    updating quantities works

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,9 +1,18 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 import { getMenu } from "../../services/apiRestaurant";
 import MenuItem from "./MenuItem";
+import { useSelector } from "react-redux";
+import { getUsername } from "../user/userSlice";
+import { useEffect } from "react";
 
 function Menu() {
   const menu = useLoaderData();
+  const navigate = useNavigate();
+  const username = useSelector(getUsername);
+
+  useEffect(() => {
+    if (!username) navigate("/");
+  }, [username]);
 
   return (
     <ul className="divide-y divide-stone-200 px-2">
